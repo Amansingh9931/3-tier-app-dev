148ad3d1ed8c60bb7d33559caa0fc0d9b7f51727
Aman Kumar <147585271+Amansingh9931@users.noreply.github.com>
2026-04-16 10:14:42 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tier.ct6ic6ksa21c.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Root123456',
+    DB_DATABASE : 'webappdb'
+});
